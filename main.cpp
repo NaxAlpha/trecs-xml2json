b64ab690aa7c57e9c3661394194dba2b093134d5
Nauman Mustafa <horizone5050@gmail.com>
2017-04-23 15:44:54 +0500

    Add source files Setup project files

---
+++ main.cpp
@@ -0,0 +1,14 @@
+// Included Libraries:
+// 
+//      TinyXML2            https://github.com/leethomason/tinyxml2/
+//      json                https://github.com/nlohmann/json
+// 
+
+
+#include "tinyxml2.h"
+#include "json.hpp"
+
+int main() {
+    
+    return 0;
+}
